a5dfe94b801450bfa6cad591193ce2cfcefb04c4
IbrohimB <botirovibrohim@gmail.com>
2023-02-28 16:48:10 +0500

    Actual Refreshment

---
+++ app/src/main/java/com/botirov/ibrohim/emptytemplate/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.botirov.ibrohim.emptytemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.botirov.ibrohim.emptytemplate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel=ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager,"newTaskTag")
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/botirov/ibrohim/emptytemplate/TaskItem.kt
@@ -0,0 +1,14 @@
+package com.botirov.ibrohim.emptytemplate
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
+
+class TaskItem(
+    var name:String,
+    var desc:String,
+    var dueTime: LocalTime?,
+    var completedDate: LocalDate?,
+    var id: UUID= UUID.randomUUID()
+    ) {
+}
